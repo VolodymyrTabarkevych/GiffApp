0a84f0309dfc0c191d676f0e4928f42fa08b2b83
VolodymyrTabarkevych <LRoolzmo357891>
2022-03-16 08:24:30 +0200

    Add font example for theming screen

--- app/src/main/java/ua/tabarkevych/composemvi/presentation/theming/screen/ThemingScreen.kt
+++ app/src/main/java/ua/tabarkevych/composemvi/presentation/theming/screen/ThemingScreen.kt
@@ -1,16 +1,18 @@
 package ua.tabarkevych.composemvi.presentation.theming.screen
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
@@ -45,15 +47,82 @@ fun ThemingScreen(
             ThemingTopBar(setEvent = setEvent)
         },
         content = {
-            Column(modifier = Modifier.padding(MaterialTheme.spacing.medium)) {
+            Column(
+                modifier = Modifier
+                    .padding(MaterialTheme.spacing.medium)
+                    .fillMaxSize()
+                    .verticalScroll(rememberScrollState())
+            ) {
                 ThemingDropdownFont(state = state, setEvent = setEvent)
                 ThemingDropdownTheme(state = state, setEvent = setEvent)
-                Image(
-                    painter = painterResource(id = R.drawable.ic_baseline_palette_24),
-                    contentDescription = null,
-                    modifier = Modifier.fillMaxSize(),
-                    colorFilter = ColorFilter.tint(color = MaterialTheme.colors.primary)
-                )
+                Row(modifier = Modifier.padding(top = MaterialTheme.spacing.medium)) {
+                    Column {
+                        Text(
+                            text = stringResource(id = R.string.font_h_1),
+                            style = MaterialTheme.typography.h1
+                        )
+                        Text(
+                            text = stringResource(id = R.string.font_h_2),
+                            style = MaterialTheme.typography.h2
+                        )
+                        Text(
+                            text = stringResource(id = R.string.font_h_3),
+                            style = MaterialTheme.typography.h3
+                        )
+                        Text(
+                            text = stringResource(id = R.string.font_h_4),
+                            style = MaterialTheme.typography.h4
+                        )
+                        Text(
+                            text = stringResource(id = R.string.font_h_5),
+                            style = MaterialTheme.typography.h5
+                        )
+                        Text(
+                            text = stringResource(id = R.string.font_h_6),
+                            style = MaterialTheme.typography.h6
+                        )
+                    }
+                    Row {
+                        Column {
+                            Text(
+                                text = stringResource(id = R.string.font_body_1),
+                                style = MaterialTheme.typography.body1
+                            )
+                            Text(
+                                text = stringResource(id = R.string.font_body_2),
+                                style = MaterialTheme.typography.body2
+                            )
+                            Text(
+                                text = stringResource(id = R.string.font_button),
+                                style = MaterialTheme.typography.button
+                            )
+                            Text(
+                                text = stringResource(id = R.string.font_caption),
+                                style = MaterialTheme.typography.caption
+                            )
+                            Text(
+                                text = stringResource(id = R.string.font_overline),
+                                style = MaterialTheme.typography.overline
+                            )
+                            Text(
+                                text = stringResource(id = R.string.font_subtitle_1),
+                                style = MaterialTheme.typography.subtitle1
+                            )
+                            Text(
+                                text = stringResource(id = R.string.font_subtitle_2),
+                                style = MaterialTheme.typography.subtitle2
+                            )
+                        }
+                    }
+                    Image(
+                        modifier = Modifier
+                            .weight(1f)
+                            .aspectRatio(1f),
+                        painter = painterResource(id = R.drawable.ic_baseline_palette_24),
+                        contentDescription = null,
+                        colorFilter = ColorFilter.tint(MaterialTheme.colors.primary)
+                    )
+                }
             }
         }
     )
